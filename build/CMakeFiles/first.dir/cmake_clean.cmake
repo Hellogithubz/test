823963e63c4797f318e595ef6f2d1fcc21d1702f
multi-master <z@1.com>
2024-05-19 18:50:44 +0800

    cit!

---
+++ build/CMakeFiles/first.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first.dir/main.cpp.obj"
+  "CMakeFiles/first.dir/main.cpp.obj.d"
+  "first.exe"
+  "first.exe.manifest"
+  "first.pdb"
+  "libfirst.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
